09c1b3d4c9b6961742566f2d0d6c13e8966118e6
Anton Plauche <antonplauche92@gmail.com>
2025-07-04 20:47:22 -0500

    remove old build files

--- build/__theme/js/editor.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '9021108f5dcbb875d839');
--- build/__theme/js/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '026209f7005ec3b0f5e4');
